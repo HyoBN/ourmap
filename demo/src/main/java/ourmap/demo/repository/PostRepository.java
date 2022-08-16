0a8940466c344f712390d962a77a78eb7befae5a
hyobn <qls0786@naver.com>
2022-08-16 18:21:09 +0900

    Spring Data JPA

--- demo/src/main/java/ourmap/demo/repository/PostRepository.java
+++ demo/src/main/java/ourmap/demo/repository/PostRepository.java
@@ -4,12 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ourmap.demo.entity.Post;
 
-import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Post save(Post post);
-    Optional<Post> findById(Long id);
-    Post findByStoreName(String name);
 
 }
