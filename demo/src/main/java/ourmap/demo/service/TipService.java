9a0a8935744188a662c7b68da0d10f4354d309d9
hyobn <qls0786@naver.com>
2022-08-17 18:09:11 +0900

    tipservice crud 구현

---
+++ demo/src/main/java/ourmap/demo/service/TipService.java
@@ -0,0 +1,35 @@
+package ourmap.demo.service;
+
+import org.springframework.stereotype.Service;
+import ourmap.demo.entity.Tip;
+import ourmap.demo.repository.TipRepository;
+
+import java.util.List;
+
+@Service
+public class TipService {
+
+    private final TipRepository tipRepository;
+
+    public TipService(TipRepository tipRepository) {
+        this.tipRepository = tipRepository;
+    }
+
+    public Long upload(Tip tip) {
+        tipRepository.save(tip);
+        return tip.getId();
+    }
+
+    public Long deleteTip(Long id) {
+        tipRepository.deleteById(id);
+        return id;
+    }
+
+    public List<Tip> findTips(){
+        return tipRepository.findAll();
+    }
+
+    public List<Tip> TipOfPost(Long postId) {
+        return tipRepository.findByPost_Id(postId);
+    }
+}[]
