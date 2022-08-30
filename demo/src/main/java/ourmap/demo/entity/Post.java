db50b1f7f5452aab90cc858090178b0faffa2df6
hyobn <qls0786@naver.com>
2022-08-30 16:44:43 +0900

    edit page를 위한 post 수정

--- demo/src/main/java/ourmap/demo/entity/Post.java
+++ demo/src/main/java/ourmap/demo/entity/Post.java
@@ -1,5 +1,6 @@
 package ourmap.demo.entity;
 
+import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.*;
@@ -17,6 +18,7 @@ public class Post {
     @Column(name="post_id")
     private Long id;
 
+    //@NotNull     에러해결하고 나중에 붙이기.
     private String storeName;
     @Enumerated(value = EnumType.STRING)
     private StoreTypes storeType; // 나중에 enum 으로 바꾸기.
@@ -25,6 +27,12 @@ public class Post {
     private List<Tip> tips = new ArrayList<>();
 
     public Post(String storeName, StoreTypes storeType) {
+        this.storeName = storeName;
+        this.storeType = storeType;
+    }
+
+    public Post(Long postId, String storeName, StoreTypes storeType) {
+        this.id=postId;
         this.storeName = storeName;
         this.storeType = storeType;
     }
