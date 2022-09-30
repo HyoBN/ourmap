a4d928c7d4116925424e8a21278ad10b9bba5469
hyoBN <qls0786@naver.com>
2022-09-30 23:24:54 +0900

    카테고리 버튼 enum으로, color값 위치 java파일로 이동

--- src/main/java/ourmap/demo/entity/StoreTypes.java
+++ src/main/java/ourmap/demo/entity/StoreTypes.java
@@ -4,14 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum StoreTypes {
-    CAFE("카페"),
-    RESTAURANT("식당"),
-    BAR("술집"),
-    OTHER("기타");
+    CAFE("카페","#FF9900"),
+    RESTAURANT("식당","#FF6600"),
+    BAR("술집","#66FFCC"),
+    OTHER("기타","#006699");
 
     private final String description;
+    private final String color;
 
-    StoreTypes(String description) {
+    StoreTypes(String description, String color) {
         this.description = description;
+        this.color = color;
     }
 }
